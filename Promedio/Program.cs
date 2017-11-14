bffecbe709e3b3a39a1c71e4e09adf23df1b384c
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-13 22:32:34 -0600

    Agregar archivos de proyecto.

---
+++ Promedio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace prog
+{
+    class Promedio
+    {
+        public static void Main(String[] args)
+        {
+            int i = 0;
+            double n = 0;
+            double h = 0;
+            double k = 1;
+            while (k != 0)
+            {
+                Console.WriteLine("Da numero");
+                n = double.Parse(Console.ReadLine());
+                if (n == 0)
+                {
+                    k = 0;
+                }
+                else
+                {
+                    h = h + n;
+                    i++;
+                }
+
+            }
+            h = h / i;
+            Console.WriteLine("R = " + h);
+            Console.ReadKey();
+        }
+
+    }
+}
